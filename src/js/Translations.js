49bae1f05d2b6b94f483dd8a901f90324d18953c
kotnikd3 <denis.kotnik@gmail.com>
2020-12-26 21:58:26 +0100

    Sprememba v besedilu uvod in mnenja

--- src/js/Translations.js
+++ src/js/Translations.js
@@ -6,17 +6,17 @@ export const translations = {
     },
     introduction : {
         'sl' : 'Moje ime je Denis Kotnik in prihajam iz Slovenije. Po izobrazbi sem računalničar, vse bolj pa me zanimata tudi filozofija in psihologija.<br><br>\
-        Navdušuje me ideja razsvetljenstva ter ljudje, ki jo zastopajo s svojim življenjem in ki iščejo samega sebe natanko zato, da bi okrepili dober, pristni, avtentični odnos do drugega. Rad prebiram besede avtorjev, kot so Sokrat, Karl Marx, Carl G. Jung, Sigmund Freud, Alain Badiou, Dušan Rutar in drugih.<br><br>\
+        Navdušuje me ideja razsvetljenstva ter ljudje, ki jo zastopajo s svojim življenjem in ki iščejo samega sebe natanko zato, da bi okrepili dober, pristni, avtentični odnos do drugega. Rad prebiram besede avtorjev, kot so Sokrat, Karl Marx, Carl G. Jung, Sigmund Freud, Alain Badiou, René Descartes in drugih.<br><br>\
         Na tej spletni strani sem se predstavil, v prihodnje pa bi rad parafraziral o idejah, do katerih sem prišel z branjem in razmišljanjem. Niso moja last, namreč idej si ne more lastiti nihče. <i>Življenje</i> je druga beseda za <i>spremembo</i>, sebe pa sem v dobrem pogovoru pripravljen postaviti pod vprašaj, zato bom zapisano s časom dopolnjeval, morda spreminjal.<br><br>\
         Upam, da bo vse to le povabilo k razmišljanju o dobrem.',
 
         'en' : 'My name is Denis Kotnik and I\'m coming from Slovenia. I hold a master\'s degree in computer science and more and more I\'m interested in philosophy and psychology.<br><br>\
-        I\'m fascinated by the idea of the Enlightenment and the people who represent it with their lives and who seek themselves precisely in order to strengthen a good, genuine, authentic relationship with another. I like to read the words of authors like Socrates, Karl Marx, Carl G. Jung, Sigmund Freud, Alain Badiou, Dušan Rutar and others.<br><br>\
+        I\'m fascinated by the idea of the Enlightenment and the people who represent it with their lives and who seek themselves precisely in order to strengthen a good, genuine, authentic relationship with another. I like to read the words of authors like Socrates, Karl Marx, Carl G. Jung, Sigmund Freud, Alain Badiou, René Descartes and others.<br><br>\
         I have introduced myself and in the future I would like to paraphrase about the ideas I came up with by reading and thinking. They are not my property, namely no one can own ideas. <i>Life</i> is another word for <i>change</i>, and in a good conversation I\'m ready to question myself, so I will supplement what is written over time, perhaps change it.<br><br>\
         I hope all of this will just be an invitation to think about the good.',
 
         'nl' : 'Mijn naam is Denis Kotnik en ik kom uit Slovenië. Ik heb een masterdiploma in computerwetenschappen en ben steeds meer geïnteresseerd in filosofie en psychologie.<br><br>\
-        Ik ben gefascineerd door het idee van de Verlichting en de mensen die het vertegenwoordigen met hun leven en die zichzelf juist zoeken om een goede, echte, authentieke relatie met een ander te versterken. Ik lees graag de woorden van auteurs als Socrates, Karl Marx, Carl G. Jung, Sigmund Freud, Alain Badiou, Dušan Rutar en anderen.<br><br>\
+        Ik ben gefascineerd door het idee van de Verlichting en de mensen die het vertegenwoordigen met hun leven en die zichzelf juist zoeken om een goede, echte, authentieke relatie met een ander te versterken. Ik lees graag de woorden van auteurs als Socrates, Karl Marx, Carl G. Jung, Sigmund Freud, Alain Badiou, René Descartes en anderen.<br><br>\
         Ik heb mezelf voorgesteld en zou in de toekomst graag willen parafraseren over de ideeën die ik door te lezen en na te denken heb bedacht. Ze zijn niet mijn eigendom, namelijk niemand kan ideeën bezitten. <i>Leven</i> is een ander woord voor <i>verandering</i>, en ik in een goed gesprek ben ik bereid mezelf in vraag te stellen, dus ik zal aanvullen wat er in de loop van de tijd is geschreven, misschien veranderen.<br><br>\
         Ik hoop dat dit allemaal slechts een uitnodiging zal zijn om na te denken over het goede.'
     },
@@ -31,7 +31,7 @@ export const translations = {
         <br>
         Sokrat pred pravico do izražanja mnenj zagovarja dolžnost do nečesa drugega. Zagovarja dolžnost do raziskovanja in razmišljanja. Do raziskovanja, v katerem je raziskovalec pripravljen sebe postavljati pod vprašaj, in razmišljanja, ki pa je delo v strogem pomenu besede. Razmišljanje je sicer univerzalno - nihče ne more narediti nič, ne da bi razmišljal - pa vendar je brez raziskovanja, kakršnega sem omenil, premalo (Hitler je namreč tudi veliko razmišljal).
         <br><br>
-        V dobi (dis)informacij, ko se mnenja generirajo na podlagi mnenj, sta raziskovanje in razmišljanje še posebej pomembna. Vedno je bilo tako. Namreč le razmišljanje lahko pripelje do tega, da se univerzalnost izrazi skozi naše partikularnosti. Le razmišljanje ustvarja mejo med ljubiteljem mnenj in ljubiteljem modrosti in ta meja je v nas samih. Le razmišljanje omejuje in posledično osvobaja človeka, ustvarja ter ločuje dobro in zlo, ki sta tudi v nas samih.
+        V dobi (dis)informacij, ko se mnenja generirajo na podlagi mnenj, sta raziskovanje in razmišljanje še posebej pomembna. Vedno je bilo tako. Namreč le razmišljanje lahko pripelje do tega, da se univerzalnost izrazi skozi naše partikularnosti. Le razmišljanje ustvarja mejo med ljubiteljem mnenj in ljubiteljem modrosti in ta meja je v nas samih. Le razmišljanje omejuje in posledično osvobaja človeka (saj je svoboda omejena z resnico), ustvarja ter ločuje dobro in zlo, ki sta tudi v nas samih.
         <br><br>
         Berem Dušana Rutarja. <i>Ptica na nebu ni svobodna; ta je le ujetnica svojih nagonov. Svoboden je človek, ko razume in dojame, kakšno je njegovo poslanstvo na tem svetu.</i>`
     }
